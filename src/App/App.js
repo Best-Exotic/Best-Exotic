05024cf4158d744805ee54c27ec8d69cd5da9be3
Leo Ware <leoware@minerva.kgi.edu>
2020-09-12 15:43:00 +0100

    added toasting capability to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,39 +1,16 @@
 import React from 'react';
+import {ToastContainer} from "react-toastify";
 import './App.css';
-import Flashcard from "../Flashcard/Flashcard";
 
-const miscDeck = [
-    {
-        frontText: "What is the capital of the UK?",
-        backText: "London"
-    },
-    {
-        frontText: "What is the largest city in the world?",
-        backText: "Tokyo"
-    },
-    {
-        frontText: "What color is the sky?",
-        backText: "blue"
-    }
-]
+import Canvas from "../Canvas/Canvas";
 
 function App() {
-
-    return(
-        <Flashcard cards={miscDeck} />
-    )
-
-  // return (
-  //   <div className="App">
-  //     <div className="viewport">
-  //       <div className="virtual-viewport">
-  //         <div id="wall">
-  //
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  return (
+      <div className={"App"}>
+          <ToastContainer />
+          <Canvas />
+      </div>
+  );
 }
 
 export default App;
